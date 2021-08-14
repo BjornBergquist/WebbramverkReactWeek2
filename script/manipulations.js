84a859095ca4545a0afa3ed1e763bb877023de71
Björn Bergquist <73097586+BjornBergquist@users.noreply.github.com>
2021-08-14 09:40:06 +0200

    Changed so that all divs have a base style.

--- script/manipulations.js
+++ script/manipulations.js
@@ -1,3 +1,14 @@
 /* Börja med att ta bort lite onödig html: */
 const parent = document.querySelector("#parent");
 parent.innerHTML = ""
+
+
+parent.innerHTML = "<div class='child'> This is a test </child>"
+
+const child = parent.children[0]
+
+child.textContent = "hej!"
+
+const div = document.createElement("div")
+
+parent.append(div)

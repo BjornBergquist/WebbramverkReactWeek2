15e66d672a0c567024d3a4db519eba7e484f0ec2
Björn Bergquist <73097586+BjornBergquist@users.noreply.github.com>
2021-08-14 09:25:26 +0200

    Update manipulations.js

--- script/manipulations.js
+++ script/manipulations.js
@@ -1,4 +1,3 @@
 /* Börja med att ta bort lite onödig html: */
 const parent = document.querySelector("#parent");
-const children = Array.from(parent.children)
-children.forEach((child) => child.remove())
+parent.innerHTML = ""

039c6d213d6a2e53b70492cc9409f4df763bc133
Björn Bergquist <73097586+BjornBergquist@users.noreply.github.com>
2021-08-19 12:11:49 +0200

    After the morning lecture.

--- script/manipulations.js
+++ script/manipulations.js
@@ -1,5 +1,13 @@
 /* Börja med att ta bort lite onödig html: */
 const parent = document.querySelector("#parent");
-parent.innerHTML = ""
+parent.innerHTML = "<div class='child'> Hej! </child>"
 
+const child = document.querySelector(".child")
 
+const grandchild = document.createElement("div")
+
+child.append(grandchild)
+
+grandchild.classList.add("red")
+grandchild.classList.add("grandchild")
+grandchild.classList.remove("red")
--- script/script.js
+++ script/script.js
@@ -3,3 +3,61 @@ som byer bakgrundsfärg på ett element*/
 const changeColor = (element) => {
    element.classList.add("red") 
 }
+
+//const parent = document.getElementById("parent")
+//changeColor(parent)
+
+//const children = Array.from(document.getElementsByClassName("child"))
+
+
+// const child3 = document.querySelector("#child-three")
+
+// const grandchildren = Array.from(children[0].children)
+
+// grandchildren.forEach((child) => changeColor(child))
+
+// const parent = grandchildren[0].closest("#parent")
+
+// changeColor(parent)
+
+// const child2 = child3.previousElementSibling
+
+//changeColor(child2)
+
+// const div = Array.from(document.getElementsByTagName("div"))
+
+const children = document.querySelectorAll(".child")
+// children.forEach((child) => child.onclick = (event) => {child = document.getElementById(id)
+//    child.classList.contains("red") ? child.classList.remove("red") : 
+//    child.classList.add("red") })
+children.forEach((child) => child.setAttribute("onclick",`myFunc(this.id)`))
+
+function myFunc(id){
+   child = document.getElementById(id)
+   child.classList.toggle("red")
+}
+
+// let active = false;
+// let children = document.getElementById("parent").children;
+
+// for(var i = 0; i < children.length; i++)
+// {
+//    children[i].onclick = function(event)
+//    {
+//       if(active == false)
+//       {
+//          for(var j = 0; j < children.length; j++)
+//          {
+//             children[j].classList.add("red");
+//          }
+//          active = true;
+//       }
+//       else
+//       {
+//          for(var j = 0; j < children.length; j++)
+//          {
+//             children[j].classList.remove("red");
+//          }
+//       }
+//    }
+// }
